69f443a65568f77bd7d973bf4cf30b51851a0e0d
fostemi <michael.isaac.foster@gmail.com>
2025-01-29 17:14:14 -0600

    create base web server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import ()
+import (
+  "fmt"
+  "net/http"
+  "log/slog"
+  "flag"
+)
+
+var (
+  addr = flag.String("addr", ":8080", "http service address")
+)
 
 func main () {
-  // run api server
+  flag.Parse()
+  http.Handle("/healthz", http.HandlerFunc(health))
+  err := http.ListenAndServe(*addr, nil)
+  if err != nil {
+    slog.Error("ListenAndServe:", slog.Any("error", err))
+  }
+}
+
+func health(w http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(w, "Healthy")
 }
---
+++ internal/api/engine.go
@@ -0,0 +1,7 @@
+package api
+
+import ()
+
+func engine() {
+
+}
