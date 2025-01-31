27555929ba2275e926bf9336895771af85902429
fostemi <michael.isaac.foster@gmail.com>
2025-01-31 17:26:03 -0600

    supply personal token

--- internal/common/bucket/controllers.go
+++ internal/common/bucket/controllers.go
@@ -1,11 +1,12 @@
 package bucket
 
 import (
-  "net/http"
-  "context"
+	"context"
+	"net/http"
 
-  // "github.com/fostemi/fdo/internal/common/utils"
+	// "github.com/fostemi/fdo/internal/common/utils"
 
+	"github.com/fostemi/fdo/internal/common/utils"
 	"github.com/gin-gonic/gin"
 
 	"cloud.google.com/go/auth"
@@ -15,7 +16,7 @@ import (
 
 func Create(c *gin.Context) error {
   ctx := context.Background()
-  creds := getCredsFromToken(c.Request)
+  creds := getCredsFromToken(ctx, getToken(c.Request))
   _, err := storage.NewClient(ctx, option.WithAuthCredentials(creds))
   if err != nil {
     return err
@@ -23,10 +24,30 @@ func Create(c *gin.Context) error {
   return nil
 }
 
-func getCredsFromToken(r *http.Request) *auth.Credentials{
-  opts := auth.CredentialsOptions{
-    TokenProvider: _,
+func getCredsFromToken(ctx context.Context, token string) *auth.Credentials{
+  t := auth.Token{
+    Value: token,
+    Type: "Bearer",
+  }
+  key := auth.Token{}
+  var tp auth.TokenProvider
+  tp.Token(context.WithValue(ctx, key, t))
+  opts := &auth.CredentialsOptions{
+    TokenProvider: tp,
   }
   creds := auth.NewCredentials(opts)
   return creds
 }
+
+func getToken(r *http.Request) string {
+  return utils.ExtractToken(r)
+}
+
+// func (tp *auth.TokenProvider) getToken(r *http.Request) auth.Token {
+//   t := utils.ExtractToken(r)
+//   token := &auth.Token{
+//     Value: t,
+//     Type: "Bearer",
+//   }
+//   return *token
+// }
