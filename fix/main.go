9dabf7c5d425265ba65da94b9b59fc8035903903
fostemi <michael.isaac.foster@gmail.com>
2025-02-07 17:35:33 -0600

    add arguments to create or delete

--- fix/main.go
+++ fix/main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"context"
-	"flag"
+  "os"
 	"fmt"
 )
 
 func main() {
-  flag.Parse()
+  args := os.Args
+  // for i, arg := range args {
+  //   fmt.Printf("Arg %d: %s\n", i, arg)
+  // }
+  // return
+  if len(args) != 2 {
+    fmt.Println("Error: Too many arguments, only accepts one")
+  }
+
   ctx := context.Background()
 
   projectID := "hydra-alert-dev-78gx"
   bucketName := "hydra-alert-dev-78gx-tst-00000"
 
-  err := createBucket(ctx, projectID, bucketName)
-  if err != nil {
-    fmt.Println("Error creating bucket: ", err)
+  switch(args[1]) {
+  case "create":
+    err := createBucket(ctx, projectID, bucketName)
+    if err != nil {
+      fmt.Println("Error creating bucket: ", err)
+    }
+    break
+  case "delete":
+    err := deleteBucket(ctx, bucketName)
+    if err != nil {
+      fmt.Println("Error deleting bucket: ", err)
+    }
+    break
+  default:
+    fmt.Println("Error: Argument not supported.")
+    break
   }
+
 }
